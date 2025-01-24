0883c83acc35e29b86af89ea146bcf3dfe216ebc
Florence Ryan <Florence.Ryan@jet2.com>
2025-01-24 08:58:24 +0000

    update linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,14 @@
 module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   root: true,
-  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
   env: {
     browser: true,
+    node: true,
+    es6: true,
   },
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    allowImportExportEverywhere: true,
-    sourceType: 'module',
-    requireConfigFile: false,
-  },
-  rules: {
-    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-    'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
-    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
-  },
+  ignorePatterns: ['node_modules', 'scripts/*.js'],
 };
